150a7432d7a5f96f6b2a1e5b2942bbbe49a6d5fd
Amit Gupta <amitgupta@Amits-MacBook-Air.local>
2024-11-22 00:22:27 +0530

    new

---
+++ 01_basics/01_dt.js
@@ -0,0 +1,28 @@
+ "use strict"; // treat all Js Code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Amit")
+
+let name = "Amit"
+let age = 22
+let isLoggedIn = false
+
+
+
+// PREMETIVE DATA TYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+//object 
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
